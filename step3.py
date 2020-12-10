cf7557b838af02a76e523e7ef1e45a9330c18edf
junamee <65053955+skawnkk@users.noreply.github.com>
2020-12-10 23:49:42 +0900

    큐브완성축하메시지 구현✨
    >DID:
    - 큐브완성시 축하 메시지와 게임정보 띄워주기
    - 동작 B와 D추가 완료
    - 코딩 전, 사전 프로세스 설계의 중요함을 깨달음
    >TODO: 무작위 큐브 섞기 기능

--- step3.py
+++ step3.py
@@ -1,4 +1,8 @@
-'''
+
+# TODO: 1.무작위 섞기 기능 추가 
+#// TODO: 2.큐브를 완성했을 때 축하 메시지 띄우기
+#// TODO: 3. B, D 함수 만들자
+''' 
 < 0 >
 B B B  
 B B B
@@ -14,44 +18,6 @@ R R R
 R R R 
 R R R 
 '''
-# -초기 큐브 및 큐브구현 함수----------------------------------------------------
-
-cube = [[] for _ in range(6)]
-for _ in range(3):
-    cube[0].append(['B','B','B'])    
-    cube[1].append(['W','W','W'])
-    cube[2].append(['O','O','O'])
-    cube[3].append(['G','G','G'])
-    cube[4].append(['Y','Y','Y'])
-    cube[5].append(['R','R','R'])
-
-
-
-def paintCube(cube):
-    
-    for i in range(3):  
-        for j in range(3):
-            print(cube[0][i][j], end="")
-        print("")
-    print()
-
-    for t in range(3):
-            for i in range(1,5):
-                for j in range(3):
-                    print(cube[i][t][j], end="")
-                print("   ", end="")
-            print()
-    print()
-
-    for i in range(3):
-        for j in range(3):
-            print(cube[5][i][j], end="")
-        print()
-    print()
-
-
-# -입력함수처리 및 동작함수----------------------------------------------------
-
 
 def linkedFace(cube,index):
 
@@ -73,13 +39,29 @@ def U(cube):
     cube[4][0] = target
     linkedFace(cube, 0)
 
+def D(cube):
+    target = cube[1][2]
+    cube[1][2] = cube[4][2]
+    cube[4][2] = cube[3][2]
+    cube[3][2] = cube[2][2]
+    cube[2][2] = target
+    linkedFace(cube, 5)
+
 def F(cube):
-    target=cube[0][2]
+    target = cube[0][2]
     cube[0][2] = [cube[4][2][2], cube[4][1][2], cube[4][0][2]]
     cube[4][2][2], cube[4][1][2], cube[4][0][2] = cube[5][0]
     cube[5][0] = [cube[2][2][0], cube[2][1][0], cube[2][0][0]]
     cube[2][0][0], cube[2][1][0], cube[2][2][0]  = target
     linkedFace(cube, 1)
+
+def B(cube):
+    target = cube[0][0]
+    cube[0][0] = [cube[2][0][2], cube[2][1][2], cube[2][2][2]]
+    cube[2][0][2], cube[2][1][2], cube[2][2][2] = cube[5][2][2], cube[5][2][1], cube[5][2][0]
+    cube[5][2][0], cube[5][2][1], cube[5][2][2] = cube[4][0][0], cube[4][1][0], cube[4][2][0]
+    cube[4][0][0], cube[4][1][0], cube[4][2][0] = target
+    linkedFace(cube, 3)
 
 def R(cube):
     target = [cube[1][0][2], cube[1][1][2], cube[1][2][2]]
@@ -101,76 +83,145 @@ def L(cube):
 def matching_func(cube, y, i):
     if y[i] == "U":
         U(cube)
+    if y[i] == "D":
+        D(cube)
     if y[i] == "F":
         F(cube)
+    if y[i] == "B":
+        B(cube)
     if y[i] == "R":
         R(cube)
     if y[i] == "L":
         L(cube)
 
+
+def checkResult(cube, start):
+     
+    if cube==perfect_cube:
+        print("축하합니다😆\n큐브를 맞췄어요!!✨\n")
+        quit(cube, start)
+    else: 
+        gameStart(cube, start)
+
 def perform_order(cube,y):
     global count
     
-    for i in range(len(y)):
+    for i in range(len(y)): #!조건 구분 간결하게 정리하기.
         
-        if (len(y)==1 and i==0) or (y[i] in alphabet and i==len(y)-1):
+        if len(y)==1 and y in alphabet:
             count+=1
             matching_func(cube, y,i)
             print(y[i])
-            paintCube(cube)
-           
-        elif len(y)>1 and i<len(y)-1:
-            if y[i] in alphabet and y[i+1] in alphabet:
+            paintCube(cube, start)
+        
+                
+        elif len(y)>1 and y[i] in alphabet and i<len(y)-1: #y+i가 존재하는 경우
+            if y[i+1] in alphabet:
                 count+=1
                 matching_func(cube,y,i)
                 print(y[i])
-                paintCube(cube)
+                paintCube(cube, start)
                     
-            elif y[i] in alphabet and y[i+1]=="'":
+            elif y[i+1]=="'":
                 count+=1
                 for _ in range(3):
                     matching_func(cube,y,i)
                 print(y[i]+y[i+1])
-                paintCube(cube)
-
-            elif y[i] in alphabet and y[i+1]=="2":
+                paintCube(cube, start)
+                
+            elif y[i+1]=="2":
                 count+=1
                 for _ in range(2):
                     matching_func(cube,y,i)
+            
                 print(y[i]+y[i+1])
-                paintCube(cube)
-    
-  
+                paintCube(cube, start)
 
+        elif len(y)>1 and y[i] in alphabet and i==len(y)-1: #y[i]가 마지막 값, y+i가 없는 경우
+            count+=1
+            matching_func(cube, y,i)
+            print(y[i])
+            paintCube(cube, start)
+
+    checkResult(cube, start)
+
+        
+def randomCube():
+    print("will be ready")
+
+def quit(cube, start):
+    end=time.time()
+    interval = time.localtime(end-start)
+    print("경과시간:", time.strftime('%M:%S', interval))
+    print("조작갯수:", count)
+    print("이용해주셔서 감사합니다. 뚜뚜뚜.")
+
+def gameStart(cube, start):
+    y = input('CUBE>').upper()
+    if y=="Q":
+        quit(cube, start)
+    else:
+        perform_order(cube, y)    
+
+def paintCube(cube, start):
+    for i in range(3):  
+        for j in range(3):
+            print(cube[0][i][j], end="")
+        print("")
+    print()
+
+    for t in range(3):
+            for i in range(1,5):
+                for j in range(3):
+                    print(cube[i][t][j], end="")
+                print("   ", end="")
+            print()
+    print()
+
+    for i in range(3):
+        for j in range(3):
+            print(cube[5][i][j], end="")
+        print()
+    print()
+
+    
 # --조건 및 입력 받는 부분----------------------------------------------------")
+
 import time
+
+color='BWOGYR'
+
+# 초기큐브
+init_cube = [[[] for _ in range(3)] for _ in range(6)]
+for j in range(3):
+    for i in range(6):
+        for _ in range(3):
+            init_cube[i][j].append(color[i])   
+
+# 완성큐브
+
+perfect_cube = [[[] for _ in range(3)] for _ in range(6)]
+for j in range(3):
+    for i in range(6):
+        for _ in range(3):
+            perfect_cube[i][j].append(color[i])    
+
 input_list=['F','R','U','B','L','D',"'","2","Q"]
 alphabet=['F','R','U','B','L','D']
-
-keepgoing=True
-paintCube(cube)
-start=time.time()
+special=["'","2",""]
+start=0
 count=0
 
-while True:
-    y = input('CUBE>').upper()
-    
-    for i in y:
-        if i not in input_list:
-            print('typing_error!')
-            keepgoing=False
-            break
-        elif i=='Q':
-            end=time.time()
-            interval = time.localtime(end-start)
-            print("경과시간:", time.strftime('%M:%S', interval))
-            print("조작갯수:", count)
-            print("이용해주셔서 감사합니다. 뚜뚜뚜.")
-            keepgoing=False
-            break
-    if keepgoing==False:
-        break
-    perform_order(cube,y)
+print("▶ 초기상태:") #초기 큐브 구현
+paintCube(init_cube, start)
 
+print("▶ Do you want Mixed Cube?")
+mix=input("Enter Y or N:").upper()
+start=time.time()
+
+if mix == "Y":
+    randomCube()
+elif mix=="N":
+    gameStart(init_cube,start)
 
 
