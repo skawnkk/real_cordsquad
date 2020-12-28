40f2b8ab7ef972027e0a4dacc92b3a186a39b587
junamee <65053955+skawnkk@users.noreply.github.com>
2020-12-28 23:39:50 +0900

    배열, 객체 값에 접근하기 연습

---
+++ ms1_making_array.js
@@ -0,0 +1,46 @@
+//*미션1
+//숫자타입으로만 구성된 요소를 뽑아 배열만들기
+//실행결과
+// ["width", "height", "hOffset", "vOffset", "size", "hOffset", "vOffset"]
+
+
+const data = {
+    "debug": "on",
+    "window": {
+        "title": "Sample Konfabulator Widget",
+        "name": "main_window",
+        "width": 500,
+        "height": 500
+    },
+    "image": {
+        "src": "Images/Sun.png",
+        "name": "sun1",
+        "hOffset": 250,
+        "vOffset": 250,
+        "alignment": "center"
+    },
+    "text": {
+        "data": "Click Here",
+        "size": 36,
+        "style": "bold",
+        "name": "text1",
+        "hOffset": 250,
+        "vOffset": 100,
+        "alignment": "center",
+        "onMouseUp": "sun1.opacity = (sun1.opacity / 100) * 90;"
+    }
+}
+
+
+
+const empty = [];
+for (key in data) {
+    const main_key = data[key];
+    for (key2 in main_key) {
+        const value = main_key[key2];
+        if (typeof (value) === 'number') {
+            empty.push(key2);
+        }
+    }
+}
+console.log(empty);
